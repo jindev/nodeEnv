86a4054f820acbce04bfe53b6303437d1b38a836
jindev <bjkim.dev@mindquake.co>
2015-02-20 12:44:56 +0900

    index 수정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+doctype html
+html
+  head(prefix="og: http://ogp.me/ns#")
+    meta(charset="utf-8")
+    meta(name="viewport" content="width=device-width, initial-scale=1")
+    title= title
+    link(rel='stylesheet' href='/bower_components/bootstrap/dist/css/bootstrap.min.css')
+    script(src='/bower_components/jquery/dist/jquery.min.js')
+    script(src='/bower_components/bootstrap/dist/js/bootstrap.min.js')
+  body
+    block content
+    include ga
+    include github_badge
